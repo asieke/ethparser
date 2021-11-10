c672cdc77a909a9ddba3519e5a4bb4743c30aafe
Alex Sieke <asieke@gmail.com>
2021-11-09 20:54:15 -0500

    added readme, finished script

---
+++ README.md
@@ -0,0 +1,26 @@
+## ETH PARSER
+
+This script scrapes the top 10,000 addresses from etherscan (https://etherscan.io/accounts/1?ps=100) into a csv.
+
+### Installation
+
+clone the repo
+
+```
+https://github.com/asieke/ethparser.git
+```
+
+from within the ethparser directory, run npm install
+
+```
+cd ethparser
+npm install
+```
+
+run the ETH parser
+
+```
+node index.js
+```
+
+your output will be in eth.csv
--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-const cheerio = require('cheerio');
-const axios = require('axios');
+const cheerio = require("cheerio");
+const axios = require("axios");
+const fs = require("fs");
 
+let getStuff = async () => {
+  await fs.writeFile("eth.csv", "", () => {});
 
-axios.get("https://etherscan.io/accounts/1?ps=100")
-  .then(function (response) {
-    // handle success
-    let $ = cheerio.load(response.data);
-    let html = $('.table').html();
+  for (let i = 1; i <= 100; i++) {
+    let response = await axios.get(
+      "https://etherscan.io/accounts/" + i + "?ps=100"
+    );
+    let $ = await cheerio.load(response.data);
+    let data = [];
+    $("td").each(function (index) {
+      data.push($(this).text());
+    });
 
-    console.log(html);
+    let out = [];
+    for (let i = 0; i < data.length; i += 6) {
+      let temp = data.slice(i, i + 6);
+      temp[3] = temp[3].slice(0, -6);
+      out.push(temp);
+    }
 
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+    console.log("Writing Addresses ", i * 100 - 99, " - ", i * 100);
+    let text = out.map((x) => '"' + x.join('","') + '"').join("\n");
+
+    await fs.appendFile("eth.csv", text, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      //done!
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+};
+
+getStuff();
